0232f8c84a2b2d90870b37cd55f407bf2229c2ed
bgreer5050@gmail.com <bgreer5050@gmail.com>
2016-05-11 10:32:17 -0500

    Notice we created a Triangle class that inherits AbstrPolygon.  Notice the triangle constructor takes a parameter of sides.  We know how many sides a triangle takes, so lets change the constructor.

---
+++ PolygonsLibrary/Triangle.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PolygonsLibrary
+{
+    public class Triangle : AbstractRegularPolygon
+    {
+        public Triangle(int sides, int length) : base(sides, length)
+        {
+        }
+
+        public override double GetArea()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
