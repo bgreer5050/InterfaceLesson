9032485c820b3c3e2f84cc27b05f7c7e7eddea50
bgreer5050@gmail.com <bgreer5050@gmail.com>
2016-05-11 09:58:03 -0500

    Created the base class for Polygon.

--- Interfaces/ConcreteRegularPolygon.cs
+++ Interfaces/ConcreteRegularPolygon.cs
@@ -9,5 +9,30 @@ namespace Interfaces
     public class ConcreteRegularPolygon
     {
 
+
+        public ConcreteRegularPolygon(int sides, int length)
+        {
+            NumberOfSides = sides;
+            SideLength = length;
+        }
+
+        public int NumberOfSides { get; set; }
+        public int SideLength { get; set; }
+
+        public double GetPerimeter()
+        {
+            return NumberOfSides * SideLength;
+        }
+
+        /// <summary>
+        /// The calculation of GetArea is different depending on how many sides the Polygon has
+        /// We will throw a not implemented exception and it will be up to the child class to override
+        /// this method.
+        /// </summary>
+        /// <returns></returns>
+        public virtual double GetArea()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
