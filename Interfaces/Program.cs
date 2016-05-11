f6ce7ec5a19f646116e5201061487215f2320d3d
bgreer5050@gmail.com <bgreer5050@gmail.com>
2016-05-11 10:15:55 -0500

    Created a polygon library.

--- Interfaces/Program.cs
+++ Interfaces/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using PolygonsLibrary;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,7 +11,20 @@ namespace Interfaces
     {
         static void Main(string[] args)
         {
-            
+            var square = new Square(5);
+            DisplayPolygon("Square", square);
+
+            Console.Read();
+        }
+
+        private static void DisplayPolygon(string strShape, dynamic shape)
+        {
+            Console.WriteLine(strShape + " Number of Sides: " + shape.NumberOfSides);
+            Console.WriteLine(strShape + " Side Length: " + shape.SideLength);
+            Console.WriteLine(strShape + " Perimiter: " + shape.GetPerimeter());
+            Console.WriteLine(strShape + " Area: " + shape.GetArea());
+
+                
         }
     }
 }
